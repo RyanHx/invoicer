da9b2221c2822df16e6f19a9d524c89ecfd772e2
RyanHx <hollick5@hotmail.com>
2023-09-05 14:12:44 +0100

    Fix ignored new lines.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = cname + "=" + encodeURIComponent(cvalue) + ";" + expires + ";path=/";
 }
 
 // https://www.w3schools.com/js/js_cookies.asp
